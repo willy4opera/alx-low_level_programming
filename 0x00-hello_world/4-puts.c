c9b4678a98406b35fdf8cd5ff0d84c808c3dc6be
willy4opea <willy4opera@gmail.com>
2023-02-16 09:52:11 -0800

    C program to output string using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *The main function - prints the phrase "Programming is like building a multilingual puzzle
+ * The main function - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
