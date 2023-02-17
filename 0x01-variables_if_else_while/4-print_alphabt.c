bb885fb54c466272afd8b1325615feddec4334d0
willy4opea <willy4opera@gmail.com>
2023-02-17 14:04:54 -0800

    Prints characters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -The main prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 and its always a (Sucess)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
