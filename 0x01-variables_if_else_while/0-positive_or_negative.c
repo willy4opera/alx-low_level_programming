0c4222665ff49c78431309ce0781c517c9174985
willy4opea <willy4opera@gmail.com>
2023-02-17 10:47:26 -0800

    The positive test random program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,17 +10,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
     if(n>0)
-    {
        printf("%d is positive ",n);
-    }
+    
     else if(n<0)
-    {
+    
        printf("%d is negative",n);
-    }
+    
     else
-    {
         printf("%d is zero",n);
-    }
+    
     
 
       
