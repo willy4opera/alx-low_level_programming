63b7fca0d09e5bd899787ce66714df1ad0495295
willy4opea <willy4opera@gmail.com>
2023-03-02 07:10:09 -0800

    Updated my _putchar function

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - This program writes the character c to stdout
+ * @c: This is the Character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
