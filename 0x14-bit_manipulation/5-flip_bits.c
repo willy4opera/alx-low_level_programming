df56c6d1e7f1b66b61dd4c0c2c44bac5234c3eb9
willy4opea <willy4opera@gmail.com>
2023-04-10 20:56:18 -0700

    A function that returns the number of bits you would need to flip to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - This function sets
+ *	the value of a bit to 0 at a given index
+ * @n: A pointer to decimal number to change
+ * @index: index position to change
+ * Return: 1 if it worked, -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int count;
+	unsigned int h_d;
+
+	if (index > 64)
+		return (-1);
+	h_d = index;
+	for (i = 1; h_d > 0; i *= 2, h_d--)
+		;
+
+	if ((*n >> index) & 1)
+		*n -= i;
+
+	return (1);
+}
