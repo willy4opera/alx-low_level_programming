274033f17fd145c7ad634c49014c0cbf8ba67891
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-21 22:31:02 -0400

    Task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/willy4opera/0x18-dynamic_libraries/blob/main/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.s
