cb6774e0d50d8e0ec927ea1948b964c1218929d6
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-12-15 05:34:33 -0500

    Debugged the 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_searchi - Using the linear search algorithm,
+ * linear_search - Using the linear search algorithm,
  * This function searches for a value in an array of
  * integers.
  *
